c68e55d7bd6e8829742490ad0fe0201a919d2664
Vishal Jaiswada <vp022570@gmail.com>
2024-10-13 12:08:36 +0530

    Added os_module

---
+++ os_module.js
@@ -0,0 +1,14 @@
+const os = require('os');
+
+// Get CPU information
+console.log(os.arch());
+//console.log(os.cpus());
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.loadavg());
+console.log(os.machine());
+// console.log(os.networkInterfaces());
+console.log(os.release());
+console.log(os.totalmem());
+console.log(os.version());
